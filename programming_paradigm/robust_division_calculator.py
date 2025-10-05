819dbe523558a17dea479cf493b5260b051c1551
deninjo <imwendwadennis@gmail.com>
2025-10-05 19:57:28 +0300

    Robust Division Calculator2

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,18 +10,22 @@ and main.py, which interfaces with the user through the command line.
 
 def safe_divide(numerator, denominator):
     try:
-        # Convert both values to floats as sys.arg reads strings
+        # convert both values to floats as sys.arg reads strings
         numerator = float(numerator)
         denominator = float(denominator)
 
+        # manually raise zero division error
         if denominator == 0:
-            raise ZeroDivisionError("Cannot divide by zero")
+            raise ZeroDivisionError
 
         result = numerator / denominator
-        return result
+        return f"The result of the division is: {result}"
 
+    # handles error instead of letting program crash.
     except ZeroDivisionError:
-        return "Cannot divide by zero."
+        return "Error: Cannot divide by zero."
+
+    # handless inappropriate values passed to a function
     except ValueError :
         return f"Invalid input type: please enter numeric values only."
 
